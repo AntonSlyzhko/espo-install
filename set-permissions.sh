da00995e06aee90920c652bac4eef82da5eb41ba
Anton Slyzhko <a.slyzhko@astracosm.com>
2024-08-05 02:20:23 +0300

    Create script for setting file permissions

---
+++ set-permissions.sh
@@ -0,0 +1,5 @@
+find . -type d -exec chmod 755 {} + && find . -type f -exec chmod 644 {} +;
+find data custom client/custom -type d -exec chmod 775 {} + && find data custom client/custom -type f -exec chmod 664 {} +;
+chmod 775 application/Espo/Modules client/modules;
+chmod 754 bin/command;
+chown -R www-data:www-data .;
