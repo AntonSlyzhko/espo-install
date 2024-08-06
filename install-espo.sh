dd64c214e778509c7f735328efa5837230c3b035
Anton Slyzhko <anton.slyzhko@gmail.com>
2024-08-07 00:25:22 +0300

    Add more automation

--- install-espo.sh
+++ install-espo.sh
@@ -3,11 +3,25 @@
 # Basic packages installation
 sudo apt update && sudo apt upgrade -y
 sudo apt install curl ca-certificates apt-transport-https gnupg2 lsb-release ubuntu-keyring nginx mariadb-server unzip -y
-sudo apt install --no-install-recommends php php-fpm php-mysql php-json php-imap php-gd php-zip php-mbstring php-curl php-xml php-bcmath php-ldap php-zmq -y
 sudo phpenmod imap mbstring
 
 # Define php version for future usage
 php_version=$(php -r 'echo PHP_VERSION;' | awk -F. '{print $1"."$2}')
+
+sudo sed -i 's/^max_execution_time =.*$/max_execution_time = 180/' "/etc/php/$php_version/cli/php.ini"
+sudo sed -i 's/^max_execution_time =.*$/max_execution_time = 180/' "/etc/php/$php_version/fpm/php.ini"
+
+sudo sed -i 's/^max_input_time =.*$/max_input_time = 180/' "/etc/php/$php_version/cli/php.ini"
+sudo sed -i 's/^max_input_time =.*$/max_input_time = 180/' "/etc/php/$php_version/fpm/php.ini"
+
+sudo sed -i 's/^memory_limit =.*$/memory_limit = 256M/' "/etc/php/$php_version/cli/php.ini"
+sudo sed -i 's/^memory_limit =.*$/memory_limit = 256M/' "/etc/php/$php_version/fpm/php.ini"
+
+sudo sed -i 's/^post_max_size =.*$/post_max_size = 128M/' "/etc/php/$php_version/cli/php.ini"
+sudo sed -i 's/^post_max_size =.*$/post_max_size = 128M/' "/etc/php/$php_version/fpm/php.ini"
+
+sudo sed -i 's/^upload_max_filesize =.*$/upload_max_filesize = 128M/' "/etc/php/$php_version/cli/php.ini"
+sudo sed -i 's/^upload_max_filesize =.*$/upload_max_filesize = 128M/' "/etc/php/$php_version/fpm/php.ini"
 
 # Core services setup
 sudo systemctl start nginx.service && sudo systemctl enable nginx.service 
@@ -24,6 +38,15 @@ DB_USER_PASS=$(openssl rand -base64 25 | tr -dc '[:alnum:]!@#$%^&*()-_+=' | head
 SQL_COMMAND="DROP DATABASE IF EXISTS espocrmdb; CREATE DATABASE espocrmdb; GRANT ALL ON espocrmdb.* TO espocrmuser@localhost IDENTIFIED BY '$DB_USER_PASS' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 sudo mariadb --user=root -e "$SQL_COMMAND"
 
+MARIADB_CONFIG="
+max_connections = 200
+table_definition_cache = 800
+table_open_cache = 4000
+innodb_flush_method = O_DIRECT
+innodb_log_file_size = 2G
+innodb_flush_log_at_trx_commit = 2
+"
+echo "$MARIADB_CONFIG" | sudo tee -a "/etc/mysql/mariadb.conf.d/50-server.cnf" > /dev/null
 
 # Fetch the latest EspoCRM release URL of the zip asset
 zip_url=$(curl -s "https://api.github.com/repos/espocrm/espocrm/releases/latest" | grep "browser_download_url" | cut -d '"' -f 4)
@@ -50,6 +73,15 @@ sudo systemctl enable espocrm-websocket.service
 sudo systemctl start espocrm-websocket.service
 sudo systemctl restart mariadb
 
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
+
+SELF_SIGNED_CERT="
+ssl_certificate /etc/ssl/certs/nginx-selfsigned.crt;
+ssl_certificate_key /etc/ssl/private/nginx-selfsigned.key;
+"
+sudo touch /etc/nginx/snippets/self-signed.conf
+
+echo "$SELF_SIGNED_CERT" | sudo tee -a "/etc/nginx/snippets/self-signed.conf" > /dev/null
 
 # Create Nginx site config
 nginx_conf_file_path="/etc/nginx/sites-available/espocrm.conf"
@@ -63,6 +95,7 @@ sudo rm /etc/nginx/sites-enabled/default
 
 sudo cp set-permissions.sh /var/www/espocrm/set-permissions.sh
 cd /var/www/espocrm && sudo find data -type d -exec sudo chmod 775 {} + && sudo chown -R 33:33 .;
-sudo sh /var/www/espocrm/set-permissions.sh
+
+
 echo "DB user: espocrmuser"
 echo "DB user password: $DB_USER_PASS"
